0b945bbb59f92464241184addda341179ec7df57
Chinnawat K <chinnawatsut@gmail.com>
2017-09-08 15:30:25 +0700

    vue life-cycle

--- vue2.js
+++ vue2.js
@@ -2,11 +2,24 @@ var data = {
   a: 1
 };
 var vm = new Vue({
+  //Do not use arrow function over here
+  //Because Arrow functions bound with parent contect
+  //It now see any in really this
   el: '#app',
-  data: data
+  data: data,
+  created: function() {
+    console.log('a is ' + this.a);
+  },
+  beforeCreate: function() {
+    console.log('before created :) ');
+  }
+
 });
 // REF.data as pointer
 
 vm.$watch('a', function(newVal, oldVal) {
   console.log(newVal);
 });
+
+console.log("More Info.");
+console.log('https://vuejs.org/v2/guide/instance.html');
