2aeb2d5800dc576fbb887272e11f5e9ee6814e4b
Chinnawat K <chinnawatsut@gmail.com>
2017-09-11 11:32:41 +0700

    attributes and component

--- vue3.js
+++ vue3.js
@@ -1,6 +1,26 @@
+Vue.component('directivx', {
+  props: ['naming', 'age', 'address', 'infos'],
+  template: "<div> {{naming}} is {{age}} years old and live in {{address}}. <br/> This is more info. <br/> {{infos}}</div>"
+});
+
 var vm = new Vue({
   el: '#app',
   data: {
     a: 3
+  }
+});
+
+var vm = new Vue({
+  el: "#app2",
+  data: {
+    anchor: "ANCHOR",
+    age: 32,
+    address: "ny",
+    img: "https://vuejs.org/images/logo.png",
+    infos: {
+      phone: "88999",
+      postcode: 22222
+    },
+    isButtonDisabled: true
   }
 });
