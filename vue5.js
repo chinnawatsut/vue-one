f96c5cbb801f92fd3a658f2c01391e9e61bde74c
Chinnawat K <chinnawatsut@gmail.com>
2017-09-11 18:23:40 +0700

    compute setted

---
+++ vue5.js
@@ -0,0 +1,22 @@
+var vm = new Vue({
+  el: "#app",
+  data: {
+    first: "makrov",
+    last: "puventin"
+  },
+  computed: {
+    fullname: {
+      get: function() {
+        return this.first + " " + this.last;
+
+      },
+      set: function(val) {
+        var names = val.split(' ');
+        this.first = names[0];
+        this.last = names[names.length - 1];
+      }
+    }
+  }
+});
+
+// vm.fullname = 'John Wane';
