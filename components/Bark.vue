864f415c060d7ade76554dc1ffb96a0dcca8d6c0
Chinnawat K <chinnawatsut@gmail.com>
2017-09-19 11:17:10 +0700

    init without eslint

---
+++ components/Bark.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>
+    Bark!!!!!
+  </h1>
+</template>
---
+++ pages/users/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+      <h1>Login :) </h1>
+      <h1 class="title">
+        Login
+      </h1>
+
+
+  </section>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Login Page'
+  },
+  meta: [{ hid: 'description', name: 'description', content: 'About page description' }]
+
+}
+</script>
+
+<style>
+
+</style>
